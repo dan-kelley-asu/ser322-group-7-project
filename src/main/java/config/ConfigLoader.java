bffc5bd9b3066abfafbd98c0044d1a7739fc1936
Dan Kelley <dan.kelley@asu.edu>
2021-07-07 20:27:13 -0400

    Fixes more issues with gradle (building JAR)

--- src/main/java/config/ConfigLoader.java
+++ src/main/java/config/ConfigLoader.java
@@ -9,7 +9,7 @@ public class ConfigLoader {
     public static void load() {
         Gson gson = new Gson();
         try {
-            JsonReader jsonReader = new JsonReader(new FileReader(ClassLoader.getSystemResource("config.json").getPath()));
+            JsonReader jsonReader = new JsonReader(new FileReader("config.json"));
             Config.config = gson.fromJson(jsonReader, Config.class);
         } catch (java.lang.Exception e) {
             System.out.println("Error loading config");
